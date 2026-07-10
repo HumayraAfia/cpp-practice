5ec87f1477b1a6fbb130d67c580f21b1c84395ed
Humayra Afia <tven1143@gmail.com>
2026-07-10 22:46:40 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string characterName = "Donald";
+    int characterAge;
+    characterAge = 80;
+    cout << "There was a man named " << characterName <<  endl;
+    cout << "He was "<<characterAge<<" years old" << endl;
+    characterName = "Epstein";
+    cout << "His best friend was "<<characterName<< endl;
+    cout << ""<<characterName<<" was a pedo" << endl;
+        return 0;
+}
